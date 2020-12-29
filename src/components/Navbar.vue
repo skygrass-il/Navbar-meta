22757fec1023539b6e43ef9e181fb0395b707068
skygrass-il <61673499+skygrass-il@users.noreply.github.com>
2020-12-29 18:51:27 +0800

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+                 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="#" @click.prevent="signout">Sign out</a>
+    </li>
+  </ul>
+</nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+    signout() {
+       const api = `${process.env.APIPATH}/logout`;
+        const vm = this;
+        this.$http.post(api).then((response)=>{
+            console.log(response.data);
+            if(response.data.success){
+                vm.$router.push('/login');
+            }
+        });
+    }
+  },
+  created() {
+         const api = `${process.env.VUE_APP_APIPATH}/api/user/check`;
+   // eslint-disable-next-line
+   this.$http.post(api).then((response) => {
+    // 登入沒有問題
+    console.log(response.data.success);
+    if (response.data.success) {
+     this.$router.push('/admin/products');
+    } else {
+      this.$router.push('/login');
+    }
+   }).catch((res) => {
+    // 驗證失敗，轉回登入頁
+    console.log(res);
+    this.$router.push('/login');
+   }); 
+  }
+}
+</script>
+
